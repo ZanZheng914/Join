167fe48a159ec7e4ab4a72149d9e86554db1842c
Zan <vans914@gmail.com>
2023-10-18 17:05:01 +0800

    更新團購網及使用者資料

---
+++ js/users.js
@@ -0,0 +1,9 @@
+var user_A={
+    companyName:"台GG",
+    department:"阿滴",
+    nickname:"多拉欸夢",
+};  
+
+document.getElementById("companyName").textContent=user_A.companyName;
+document.getElementById("department").textContent=user_A.department;
+document.getElementById("nickname").textContent=user_A.nickname;
