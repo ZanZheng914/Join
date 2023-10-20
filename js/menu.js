a00b71b6d3aa6a199993ab7e3bdb1120d7c3a4a4
Zan <vans914@gmail.com>
2023-10-20 21:00:26 +0800

    新增點餐頁面並完成點餐頁面並完成SJON

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+var eightteas = {
+    "eightteas" : [
+{"name":"請選擇一種飲料","price": null},
+{"name":"紅茶","price":25},
+{"name":"綠茶","price":25},
+{"name":"八曜和茶","price":35},
+{"name":"奶茶","price":40},
+{"name":"珍珠奶茶","price":50},
+]
+}
--- js/menu1.js
+++
@@ -1,5 +0,0 @@
-var eighttea =[
-    {eighttea_name:'紅茶',eighttea_price:30},
-    {eighttea_name:'綠茶',eighttea_price:30},
-    {eighttea_name:'八曜和茶',eighttea_price:35},
-]
