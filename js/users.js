76387bd4ba1a56fb31966895cf483c676207dca9
Zan <vans914@gmail.com>
2023-10-22 20:47:35 +0800

    新增點餐頁面user連結資訊與功能

--- js/users.js
+++ js/users.js
@@ -1,9 +1,53 @@
-var user_A={
-    companyName:"台GG-台中廠",
-    department:"R滴",
-    nickname:"馬斯克",
-};  
+// var user_A={
+//     companyName:"台GG-台中廠",
+//     department:"R滴",
+//     nickname:"馬斯克",
+// };  
 
-document.getElementById("companyName").textContent=user_A.companyName;
-document.getElementById("department").textContent=user_A.department;
-document.getElementById("nickname").textContent=user_A.nickname;
+
+// var user_B={
+//     companyName:"台GG-台中廠",
+//     department:"R滴",
+//     nickname:"皮卡丘",
+//     tel:"0912345678",
+// };  
+
+$(document).ready(function(){
+  var users = {
+    "user_A": {
+      companyName: "台GG-台中廠",
+      department: "R滴",
+      nickname: "馬斯克",
+      tel:"04-3345678"
+    },
+    "user_B": {
+      companyName: "台GG-台中廠",
+      department: "客服部",
+      nickname: "皮卡丘",
+      tel: "0912345678"
+    }
+  };
+
+  $(".user-select").on("change", function(){
+    var selectedUser = $(this).val();
+    var user = users[selectedUser];
+    if(user){
+      $("#userCompanyName").text(user.companyName);
+      $("#userDepartment").text(user.department);
+      $("#userNickname").text(user.nickname);
+      $("#userTel").text(user.tel);
+    } else {
+      $("#userCompanyName").text("");
+      $("#userDepartment").text("");
+      $("#userNickname").text("");
+      $("#userTel").text("");
+    }
+  });
+});
+
+// document.getElementById("user_A_companyName").textContent=user.user_A.companyName
+// document.getElementById("user_A_department").textContent=user.user_A.department;
+// document.getElementById("user_A_nickname").textContent=user.user_A.nickname;
+// document.getElementById("user_B_department").textContent=user.user_B.department;
+// document.getElementById("user_B_nickname").textContent=user.user_B.nickname;
+// document.getElementById("user_B_tel").textContent=user.user_B.tel;
