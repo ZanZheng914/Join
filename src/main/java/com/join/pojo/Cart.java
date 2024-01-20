b831af0303bdfad53e370be020787d3cd24e61dd
ZanZheng914 <vans914@gmail.com>
2024-01-20 14:48:40 +0800

    新增data註解

--- src/main/java/com/join/pojo/Cart.java
+++ src/main/java/com/join/pojo/Cart.java
@@ -1,8 +1,13 @@
 package com.join.pojo;
 
-import java.util.List;
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
 	private Integer cartId;
 	private Integer userId;
--- src/main/java/com/join/pojo/OrderItem.java
+++ src/main/java/com/join/pojo/OrderItem.java
@@ -1,5 +1,12 @@
 package com.join.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
 	private Integer orderItemId;
 	private Integer orderId;
