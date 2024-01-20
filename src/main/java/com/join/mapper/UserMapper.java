97ec68c6a68ee886a2d7666a4e21693b36b7d65a
ZanZheng914 <vans914@gmail.com>
2024-01-20 20:11:10 +0800

    UserMapper done

--- src/main/java/com/join/mapper/UserMapper.java
+++ src/main/java/com/join/mapper/UserMapper.java
@@ -1,5 +1,45 @@
 package com.join.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.join.pojo.User;
+
+@Mapper
 public interface UserMapper {
 	//資料庫操作，註冊新使用者、驗證登入
+	
+	@Select("SELECT * FROM user")
+	List<User> list();
+	
+	//新增用戶
+	@Insert("INSERT INTO user(username, password, name, email, gender, company, dept, createTime) " +
+            "VALUES(#{username}, #{password}, #{name}, #{email}, #{gender}, #{company}, #{dept}, #{createTime})")
+	void insertUesr(User user);
+
+	//修改用戶 by userId
+	@Update("UPDATE user SET username = #{username}, password = #{password}, name = #{name}, " +
+            "email = #{email}, gender = #{gender}, company = #{company}, dept = #{dept}, createTime = #{createTime} " +
+            "WHERE userId = #{userId}")
+	void updateUser(User user);
+	
+	//刪除用戶
+	@Delete("DELETE FROM user WHERE userId=#{userId}")
+	void deleteUser(Integer userId);
+
+
+	//查詢用戶
+	@Select("SELECT * FROM user WHERE userId=#{userId)")
+	User getUserById(Integer userId);
+
+
+
+
+
+
 }
