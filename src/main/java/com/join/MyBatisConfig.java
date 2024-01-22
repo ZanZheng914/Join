b6504648ae8214b46fa09d6a8236647714432c5b
ZanZheng914 <vans914@gmail.com>
2024-01-22 21:02:43 +0800

    新增MyBatisConfig

---
+++ src/main/java/com/join/MyBatisConfig.java
@@ -0,0 +1,34 @@
+package com.join;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan("com.join.mapper")
+public class MyBatisConfig {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory()throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		
+		//設置MyBatis配置
+		PathMatchingResourcePatternResolver resolver= new PathMatchingResourcePatternResolver();
+		sqlSessionFactoryBean.setConfigLocation(resolver.getResource("classpath:mybatis-config"));
+		
+		sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/com/join/mapper/*.xml"));
+		
+		return sqlSessionFactoryBean.getObject(); 
+	}
+	
+}
