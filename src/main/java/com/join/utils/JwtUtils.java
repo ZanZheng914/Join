8d9e890707716bcdc92364e154f5ff1d0d3f79d2
ZanZheng914 <vans914@gmail.com>
2024-01-23 22:50:29 +0800

    修改mysql與jwt的pom 註冊與登入成功

--- src/main/java/com/join/utils/JwtUtils.java
+++ src/main/java/com/join/utils/JwtUtils.java
@@ -3,12 +3,15 @@ package com.join.utils;
 import java.util.Date;
 import java.util.Map;
 
+import javax.crypto.SecretKey;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtUtils {
-	private static String signKey="zan";
+	private static SecretKey secretKey=Keys.secretKeyFor(SignatureAlgorithm.HS256);
 	private static Long expire = 43200000L;
 	
 	//生成jwt令牌
@@ -16,15 +19,16 @@ public class JwtUtils {
 				
 		String jwt=Jwts.builder()
 			.addClaims(claims)
-			.signWith(SignatureAlgorithm.HS256, signKey)
+			.signWith(secretKey)
 			.setExpiration(new Date(System.currentTimeMillis()+expire))
 			.compact();
 		return jwt;
 	}
 	//解析jwt令牌
 	public static Claims parseJWT(String jwt) {
-		Claims claims = Jwts.parser()
-				.setSigningKey(signKey)
+		Claims claims = Jwts.parserBuilder()
+				.setSigningKey(secretKey)
+				.build()
 				.parseClaimsJws(jwt)
 				.getBody();
 		return claims;
