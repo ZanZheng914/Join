95cdeb66b2f67ff3da0cd4cc17c3e612a40feb65
ZanZheng914 <vans914@gmail.com>
2024-01-23 19:37:33 +0800

    校正User setter

--- src/main/java/com/join/pojo/User.java
+++ src/main/java/com/join/pojo/User.java
@@ -30,6 +30,10 @@ public class User {
 	public String getUsername() {
 		return username;
 	}
+	
+	public void setUsername(String username) {
+		this.username=username;
+	}
 
 	public String getPassword() {
 		return password;
