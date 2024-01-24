4c1a5b160b9ee886d98ff620f4172d3b0c2a6bff
ZanZheng914 <vans914@gmail.com>
2024-01-25 00:39:20 +0800

    待處理shopId呼叫與productName

--- src/main/java/com/join/pojo/Product.java
+++ src/main/java/com/join/pojo/Product.java
@@ -12,4 +12,30 @@ public class Product {
 	private Integer shopId;
 	private String productName;
 	private Integer price;
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public Integer getShopId() {
+		return shopId;
+	}
+	public void setShopId(Integer shopId) {
+		this.shopId = shopId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	
 }
